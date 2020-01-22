3b9c339295eef4343ffb4ac7c820eb370b9a943f
Recolic Keghart <root@recolic.net>
2020-01-22 06:59:10 -0800

    ignore config.py

--- summerize-part-list/config.py
+++ summerize-part-list/config.py
@@ -5,7 +5,7 @@ allow_part_tree_reference = False
 allow_part_recursive_reference = False # Not supported YET!
 tmp_prefix = '_python_tmp_prefix_'
 
-file_encoding = 'utf8'
+file_encoding = 'gb2312'
 
 search_only_top_level_directory = False
 output_dirname = 'output'
