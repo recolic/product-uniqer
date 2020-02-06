c68ade0a7c0dc83627a9cdaf5246e551fb555ab2
Recolic Keghart <root@recolic.net>
2020-02-05 22:55:42 -0800

    copy paste tool done

---
+++ xls-copy-paste/config.py
@@ -0,0 +1,18 @@
+
+# all index starts from 1 here, because of the fucking silly xlsx.
+src_title_ULcorner = 2,1
+src_ULcorner = 3,1
+dst_title_ULcorner = 2,1
+dst_ULcorner = 3,1
+
+# src may have cols: [A,C,F,G,H], [C,E,F,G], ...
+src_cols = 3
+# dst may have cols: [A,B,C,D,E,F,G,H,I,J]
+dst_cols = 7
+
+src_dir_path = 'test/inputs/'
+template_filename = 'test/template.xlsx'
+dst_filename = 'test/output.xlsx'
+
+daemon_loop_interval = 5
+
--- xls-copy-paste/main.py
+++ xls-copy-paste/main.py
@@ -1,10 +1,9 @@
 
 import openpyxl
 import config
-import os
+import os, time, logging
 
 # https://yagisanatode.com/2017/11/18/copy-and-paste-ranges-in-excel-with-openpyxl-and-python-3/
-# https://stackoverflow.com/questions/35823835/reading-excel-file-is-magnitudes-slower-using-openpyxl-compared-to-xlrd
 
 #Copy range of cells as a nested list
 #Takes: start cell, end cell, and sheet you want to copy from.
@@ -51,48 +50,58 @@ def enlarge_2darray_by_title(input_title, output_title, input_2darray):
         return out_arr
     return [conv_1darr(arr) for arr in input_2darray]
 
+def get_flist(rootdir):
+    result = []
+    for (dirpath, dirnames, filenames) in os.walk(rootdir):
+        result += [dirpath + os.path.sep + fname for fname in filenames]
+    return set(result)
+
 # (x,y) means (rowIndex, colIndex)
 all_copied_data = []
-def process_all():
+def process_all(flist):
     global all_copied_data
+    all_copied_data = []
+
     output_xls = openpyxl.load_workbook(config.template_filename)
     output_xls_sheet = output_xls.active
     output_title = copyRange(config.dst_title_ULcorner[0], config.dst_title_ULcorner[1], config.dst_title_ULcorner[0]+1, config.dst_title_ULcorner[1]+config.dst_cols, output_xls_sheet)[0]
 
-    all_copied_data = []
     def process_one_src(fname):
         global all_copied_data
-        print('Working on file ', fname)
+        logging.info('Working on file ' + fname)
         input_sheet = openpyxl.load_workbook(fname).active
         x,y = init_x,init_y = config.src_ULcorner
         while input_sheet.cell(x, y).value != None and input_sheet.cell(x, y).value != '':
             x += 1
-        print('debug:', init_x, init_y, x, y)
         copied = copyRange(init_x, init_y, x, y+config.src_cols, input_sheet)
 
         input_title = copyRange(config.src_title_ULcorner[0], config.src_title_ULcorner[1], config.src_title_ULcorner[0]+1, config.src_title_ULcorner[1]+config.src_cols, input_sheet)[0]
         all_copied_data += enlarge_2darray_by_title(input_title, output_title, copied)
 
     # iterate over files
-    for (dirpath, dirnames, filenames) in os.walk(config.src_dir_path):
-        for fname in filenames:
-            process_one_src(dirpath + os.path.sep + fname)
+    for fname in flist:
+        process_one_src(fname)
 
-    print(all_copied_data)
+    logging.info(all_copied_data)
     if len(all_copied_data) > 0:
         output_x, output_y = config.dst_ULcorner # starts from 1
         pasteRange(output_x, output_y, output_x + len(all_copied_data), output_y + len(all_copied_data[0]), output_xls_sheet, all_copied_data)
 
     output_xls.save(config.dst_filename)
 
-
-
-
-
-
-
 def main(argv):
-    process_all()
+    logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)
+    logging.info('Daemon running...')
+    prev_flist = set()
+    while True:
+        flist = get_flist(config.src_dir_path)
+        if prev_flist != flist:
+            logging.info('Detected file change. Re-calculating...')
+            process_all(flist)
+        prev_flist = flist
+
+        time.sleep(config.daemon_loop_interval)
+
 
 
 
