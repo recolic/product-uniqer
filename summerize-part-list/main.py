0e86c23da0db7759401c3e8ddaea317807b6f61a
Recolic Keghart <root@recolic.net>
2020-02-07 06:54:47 -0800

    push this silly project to public domain

--- summerize-part-list/main.py
+++ summerize-part-list/main.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
 # A script to deal with product sheet. (see test-*.xlsx)
-# Copyright (C) 2020  Recolic Keghart <root@recolic.net>
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 # Workaround for fucking Windows NT
 import traceback
 def show_exception_and_exit(exc_type, exc_value, tb):
