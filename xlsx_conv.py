282f2433d599bd6382309785070c6402777f3002
Recolic Keghart <root@recolic.net>
2020-07-22 21:26:21 +0800

    bug fix: fucking python num conv error

--- xlsx_conv.py
+++ xlsx_conv.py
@@ -4,10 +4,22 @@ import csv, io
 def xlsx2csv(xlsxPath, sheetIndex, outputFd):
     wb = xlrd.open_workbook(xlsxPath)
     sh = wb.sheet_by_index(sheetIndex)
-    wr = csv.writer(outputFd, quoting=csv.QUOTE_NONNUMERIC)
+    wr = csv.writer(outputFd, quoting=csv.QUOTE_MINIMAL)
+
+    def fuck_number_ele(ele):
+        if type(ele) is float:
+            s = str(ele)
+            if 'e' in s.lower():
+                raise RuntimeError('Fuck python Float2Str: THIS IS A BUG. PLEASE REPORT.')
+            if s.endswith('.0'):
+                return s[:-2]
+            else:
+                return s
+        else:
+            return ele
 
     for rownum in range(sh.nrows):
-        wr.writerow(sh.row_values(rownum))
+        wr.writerow([fuck_number_ele(ele) for ele in sh.row_values(rownum)])
 
 def read_as_csv(fname):
     is_xlsx = lambda fname: fname.endswith('.xlsm') or fname.endswith('.xlsx') or fname.endswith('.xls') or fname.endswith('.XLSM') or fname.endswith('.XLSX') or fname.endswith('.XLS')
